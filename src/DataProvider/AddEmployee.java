5aa4d21f713da1f6e74193848b3e5912a3f7bc9c
amolu <amolujagare@gmail.com>
2023-02-16 09:07:22 +0530

    selenium

--- src/DataProvider/AddEmployee.java
+++ src/DataProvider/AddEmployee.java
@@ -1,6 +1,5 @@
 package DataProvider;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -8,35 +7,44 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.Duration;
 
-public class LoginDataProviderEx {
-
-    @Test (dataProvider = "getData")
-    public void myTest1(String username,String password)
-    {
-        WebDriver driver = new ChromeDriver();
+public class AddEmployee {
+    WebDriver driver ;
+    @BeforeClass
+    public void doLogin() {
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/");
-
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-
         WebElement txtUsername = driver.findElement(By.name("username"));
-        txtUsername.sendKeys(username);
-
+        txtUsername.sendKeys("Admin");
         WebElement txtPassword = driver.findElement(By.name("password"));
-        txtPassword.sendKeys(password);
-
+        txtPassword.sendKeys("admin123");
         WebElement btnLogin = driver.findElement(By.xpath("//button[@type='submit']"));
         btnLogin.click();
+        driver.findElement(By.xpath("//span[contains(@class,'main-menu') and normalize-space()='PIM']")).click();
     }
 
+
+    @Test(dataProvider = "getData")
+    public void addEmpTest(String fName,String mName,String lName,String id) throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.findElement(By.xpath(" //a[normalize-space()='Add Employee']")).click();
+        driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys(fName); // First name
+        driver.findElement(By.xpath("//input[@placeholder='Middle Name']")).sendKeys(mName); // middle name
+        driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys(lName); // Last name
+        driver.findElement(By.xpath("//div[@class='oxd-input-group oxd-input-field-bottom-space']//div//input[@class='oxd-input oxd-input--active']"))
+                .sendKeys(id); //id
+        driver.findElement(By.xpath("//button[normalize-space()='Save']")).click();
+        Thread.sleep(3000);
+    }
 
     @DataProvider
     public Object[][] getData() throws IOException {
@@ -45,36 +53,23 @@ public class LoginDataProviderEx {
         //2. convert file object into workbook object
         HSSFWorkbook workbook = new HSSFWorkbook(fis);
         //3. choose sheet
-        HSSFSheet sheet = workbook.getSheet("Sheet1");
+        HSSFSheet sheet = workbook.getSheet("Add Employee");
         int rowCount = sheet.getPhysicalNumberOfRows();
         int colCount = sheet.getRow(0).getLastCellNum();
 
-        Object[][] data = new Object[rowCount][2];
+        Object[][] data = new Object[rowCount][colCount];
 
-        for (int i=0;i<rowCount;i++)
+        for (int i=0;i<rowCount-1;i++)
         {
             HSSFRow row = sheet.getRow(i);
 
-           // HSSFCell cell0 = row.getCell(0);
-
             data[i][0] = row.getCell(0).toString().trim();
             data[i][1] = row.getCell(1).toString().trim();
+            data[i][2] = row.getCell(2).toString().trim();
+            data[i][3] = row.getCell(3).toString().trim();
 
         }
 
-        /*data[0][0] = "Admin";
-        data[0][1] = "admin123";
-
-        data[1][0] = "pqr";
-        data[1][1] = "xyz";
-
-        data[2][0] = "aaa";
-        data[2][1] = "ppp";
-
-        data[3][0] = "amol";
-        data[3][1] = "amol12";*/
-
         return data;
     }
-
 }
